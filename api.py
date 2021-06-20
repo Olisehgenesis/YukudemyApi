644080221a13df846f4bea66f40f1ab75fa5fa94
Olisehgenesis <61495902+Olisehgenesis@users.noreply.github.com>
2021-06-20 01:37:43 -0700

    Update Api.py
    fixed all issues
    added a word count for key words

--- api.py
+++ api.py
@@ -14,12 +14,22 @@ headers = {
 
 #body
 article_title = str(input("Article Title:"))
-key_points = str(input("Key Points Atleast 16 Word:"))
+for key_words in range(0,15):
+     key_points = str(input(" Please Enter Key Points Above 15 Word:"))
+     key_words = len(key_points.split())
+     if (key_words > 15):
+         break
 article_content = str(input("Body :"))
 article_content_format="html"
 tags= str(input("Tags:"))
 eyrie_id=str(int(input("Eyrie_id:")))
-payload = "article_title="+article_title+"&"+"key_point="+key_points+"&"+"article_content="+article_content+"&"+"article_content_format=html"+"tags="+tags+"&"+"eyrie_id="+eyrie_id
+payload={ "article_title":article_title,
+          "article_content":article_content,
+          "key_points":key_points,
+          "article_content_format":"html",
+          "tags":tags,
+          "eyrie_id":eyrie_id }
+
 
 #request
 response = requests.request(method="POST" ,url=Url,data=payload,headers=headers)
